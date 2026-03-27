36de7a585ab8d8e319310ab2322df8d8c11ab754
code2026 <wmywqd@gmail.com>
2026-03-27 21:23:31 +0800

    second

--- springboot/src/main/resources/sql/migrations/20260327_alter_student_gpa_risk_level.sql
+++ springboot/src/main/resources/sql/migrations/20260327_alter_student_gpa_risk_level.sql
@@ -1,4 +1,5 @@
 -- Upgrade script for existing databases that still use ENUM/HIGH-MEDIUM-LOW style values.
 -- Run this once on the study_warning database.
+USE study_warning;
 ALTER TABLE student_gpa
 MODIFY COLUMN risk_level VARCHAR(20) NOT NULL;
